9aa834ef43515fafdb3357ed9c5a2791f29f877b
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2023-08-11 02:07:04 +0500

    Famms's website unresponsive yet

---
+++ app.js
@@ -0,0 +1,10 @@
+const head = document.getElementById("nav-item2")
+
+function dropDown(){
+    const content = document.querySelector(".dropDownContent")
+    content.classList.toggle("show")
+    console.log("working")
+}
+
+
+
